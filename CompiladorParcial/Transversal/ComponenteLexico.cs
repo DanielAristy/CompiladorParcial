3aa4001b7be5d1cc2952ef0cbc7fc0628a66c53f
DanielAristy <danielariaty@gmail.com>
2020-04-16 21:03:19 -0500

    Creando clase Categoria, ComponenteLexico y TipoComponente

---
+++ CompiladorParcial/Transversal/ComponenteLexico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompiladorParcial.Transversal
+{
+    class ComponenteLexico
+    {
+        public String Lexema { get; set; }
+        public Categoria Categoria { get; set; }
+        public int NumeroLinea { get; set; }
+        public int PosicionIncial { get; set; }
+        public int PosicionFinal { get; set; }
+        public TipoComponente Tipo { get; set; }
+
+    }
+}
